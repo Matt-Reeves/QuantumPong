82a4d075e47e6e9e22fe68cc0835a9dd506edc5a
Matt Reeves <mattreeves@uqmreev4.vpn.uq.edu.au>
2018-04-07 13:17:49 +1000

    Initial Commit

---
+++ QuantumPong2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuantumPong2
+//
+//  Created by Matt Reeves on 7/04/18.
+//  Copyright © 2018 Matt Reeves. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
